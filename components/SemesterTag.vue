6860ec85a4ed0244fc38bb0df240dfdd3a5d9b37
JHIH-SIOU LI <jsli@linux.com>
2024-06-14 20:41:56 +0800

    update semester tag.

---
+++ components/SemesterTag.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { computed } from 'vue';
+import semester from '~/semesterConfig.json';
+
+const semesterTag = computed(() => `${semester.YEAR} 學年度 第 ${semester.SEMESTER} 學期`);
+const semesterTagUrl = computed(() => `https://course.thu.edu.tw/view-dept/${semester.YEAR}/${semester.SEMESTER}`);
+</script>
+<template>
+  <a id="semester-tag" class="navbar-item" :href="semesterTagUrl" target="_blank" rel="noopener noreferrer">
+    {{ semesterTag }}
+  </a>
+</template>
